acec8051c573a1d23fa877aaee39bc2a76a4431e
NikolaKasabov <n_kasabov@mail.bg>
2023-10-29 21:05:55 +0200

    add section 6

--- travel/assets/js/app.js
+++ travel/assets/js/app.js
@@ -2,12 +2,14 @@ const nav = document.querySelector('header nav');
 const openNavBtn = document.getElementById('open-nav-btn');
 const closeNavBtn = document.getElementById('close-nav-btn');
 const navLinks = document.querySelectorAll('header nav a');
+const accordionItems = document.querySelectorAll('.section-6 .left .item');
+const toggleAccordionButtons = document.querySelectorAll('.section-6 .left .toggle-btn');
 
 openNavBtn.addEventListener('click', () => {
   openNav();
 });
 
-closeNavBtn.addEventListener('click', () => { 
+closeNavBtn.addEventListener('click', () => {
   closeNav();
 });
 
@@ -23,4 +25,21 @@ function openNav() {
 
 function closeNav() {
   nav.classList.remove('open');
+}
+
+toggleAccordionButtons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const isAccordionOpen = btn.closest('.item.active');
+
+    if (isAccordionOpen) {
+      closeAllAccordionItems();
+    } else {
+      closeAllAccordionItems();
+      btn.closest('.item').classList.add('active');
+    }
+  });
+});
+
+function closeAllAccordionItems() {
+  accordionItems.forEach(item => item.classList.remove('active'));
 }
