2968dfd48bb13f39b53a4bce6b0b8681e17d690b
NikolaKasabov <n_kasabov@mail.bg>
2024-01-13 18:59:20 +0200

    nav link click closes mobile nav

--- burgers-wide/assets/js/script.js
+++ burgers-wide/assets/js/script.js
@@ -1,11 +1,24 @@
 const nav = document.querySelector('header nav');
+const navLinks = document.querySelectorAll('header nav a');
 const openNavBtn = document.getElementById('open-nav-btn');
 const closeNavBtn = document.getElementById('close-nav-btn');
 
-openNavBtn.addEventListener('click', () => {
+function openNav() {
   nav.classList.add('open');
+}
+
+function closeNav() {
+  nav.classList.remove('open');
+}
+
+navLinks.forEach(link => {
+  link.addEventListener('click', closeNav);
+});
+
+openNavBtn.addEventListener('click', () => {
+  openNav();
 });
 
 closeNavBtn.addEventListener('click', () => {
-  nav.classList.remove('open');
+  closeNav();
 });
